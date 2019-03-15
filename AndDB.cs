89b700609e09a99d355af2f606a3b23caa2789d8
Taner <47195442+tanerurun@users.noreply.github.com>
2019-03-15 04:23:52 +0300

    Add files via upload

---
+++ AndDB.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using And.Eticaret.Core.Model.Entity;
+
+namespace And.Eticaret.Core.Model
+{
+   public class AndDB:DbContext
+       
+    {//ana database buarası
+        //buarda database class işlemleri yapıyoruz buarası database işlemleri 
+        //yapıldıı yer  
+        //database bu classa erieşecek dbcontext eklediğimiz icin
+        //bundan sonra tablolar burada olacak (Model kendisi ekliyor fakat biz oluşturup ekliyoruz)farkı budur
+        public AndDB()
+            : base(@"Data Source=.;Initial Catalog=AndEticaretDB;Integrated Security=True")
+        {
+
+        }
+
+        //burası bizim anadatabasemiz olduğu için oluşturduğumu tabloları bu şekilde buraya taşıyoruz
+
+        public DbSet<User> Users { get; set; }       
+        public DbSet<UserAddress> UserAddresses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+        public DbSet<OrderPayment> OrderPayments { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ EntityBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace And.Eticaret.Core.Model
+{
+    /*bütün tablolarda var yani ortak olan özellikleri bu sinifa tanımlıyoruz ve bunu miras olarak veriyoruz
+     hepsine tekrar tek tek tablolarda yazmamıza gerek yok */
+
+   public class EntityBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int ID { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int CreateUserID { get; set; }
+
+        public DateTime? UpdateDate { get; set; }
+        public int? UpdateUserID { get; set; }
+    }
+}
